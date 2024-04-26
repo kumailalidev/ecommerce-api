48e2e0c727523fa7b0ebf1e0465ab5cb88f056a7
kumailalidev <kumailalidev@gmail.com>
2024-04-26 23:30:05 +0500

    Added ability to customize Django admin with custom CSS file.

--- project/core/admin.py
+++ project/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy
 
-# Register your models here.
+# overriding default AdminSite instance 'admin_site' properties
+
+# Text to put at the end of each page's <title>.
+admin.site.site_title = gettext_lazy("eCommerce API")
+
+# Text to put in each page's <h1>.
+admin.site.site_header = gettext_lazy("eCommerce API Administration")
+
+# Text to put at the top of the admin index page.
+admin.site.index_title = gettext_lazy("Administration")
